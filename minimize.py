92d1bda9bb0f7feb88c655cdebb8d282af29cc8d
Gabriel Valle <gabrielmvalle@gmail.com>
2019-10-15 18:05:35 -0300

    Move minimizer class to new module

---
+++ minimize.py
@@ -0,0 +1,71 @@
+import numpy as np
+class Minimizer:
+    def __init__(self, f, inputs, x_0 = np.array([0,0]), alpha = 0.1):
+        self.f = f
+        self.inputs = inputs
+        self.f_grad = None
+        self.f_hess = None
+        self.x = [x_0]
+        self.steps = []
+        self.alpha = alpha
+        self.B = []
+
+    def line_search(self, dr):
+        t = 1
+        x = self.x[-1]
+        #Armijo condition
+        while self.f(x + t*dr) >= self.f(x) + self.alpha*t*np.inner(self.f_grad(x), dr):
+            if t == 0:
+                print(dr, f(x), self.alpha*t*np.inner(self.f_grad(x), dr))
+                print('line_search failed')
+                break
+            t /= 2
+        return t
+
+    def gradient_step(self):
+        x = self.x[-1]
+        direction = -self.f_grad(x)
+        t = self.line_search(direction)
+        print('t =', t)
+        self.steps.append(t*direction)
+        self.x.append(self.x[-1]+self.steps[-1])
+        return t*direction
+        #gradient method
+    def newton_step(self):
+        x = self.x[-1]
+        step = np.linalg.solve(self.f_hess(x), -self.f_grad(x))
+        self.steps.append(step)
+        self.x.append(self.x[-1]+self.steps[-1])
+        return step
+    def BFGS_step(self):
+        x = self.x[-1]
+        direction = np.linalg.solve(self.B[-1], -self.f_grad(x))
+        t = self.line_search(direction)
+        step = t*direction
+        self.steps.append(step)
+        self.x.append(self.x[-1]+self.steps[-1])
+        y = self.f_grad(self.x[-1]) - self.f_grad(self.x[-2])
+        B_ = self.B[-1]
+        B_0 = np.outer(y,y)/np.inner(y, step)
+        B_1 = - np.outer(B_ @ step, B_ @ step)/(np.transpose(step) @ B_ @ step)
+        #computes next Hessian approximation
+        self.B.append(B_ + B_0 + B_1)
+        return step
+
+    def step(self, method = 'default'):
+        if method == 'default' and self.f_hess != None:
+            if np.array_equal(self.f_hess(self.x[-1]), np.transpose(self.f_hess(self.x[-1]))):
+                try:
+                    np.linalg.cholesky(self.f_hess(self.x[-1]))
+                    method = 'newton'
+                except np.linalg.LinAlgError:
+                    print('non-positive definite matrix')
+        if method == 'BFGS':
+            if len(self.B) == 0:
+                self.B.append(np.identity(self.inputs))
+            self.BFGS_step()
+        elif method == 'newton':
+            self.newton_step()
+        else: # gradient descent is default
+            self.gradient_step()
+        return (self.steps[-1], method)
--- proj.py
+++ proj.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import math
 import sys
+import minimize as mini
 
 delta = 10e-4
 if len(sys.argv) > 1:
@@ -12,76 +13,6 @@ else:
     method = "newton"
 
 print('method:',method)
-class Minimizer:
-    def __init__(self, f, inputs, x_0 = np.array([0,0]), alpha = 0.1):
-        self.f = f
-        self.inputs = inputs
-        self.f_grad = None
-        self.f_hess = None
-        self.x = [x_0]
-        self.steps = []
-        self.alpha = alpha
-        self.B = []
-
-    def line_search(self, dr):
-        t = 1
-        x = self.x[-1]
-        #Armijo condition
-        while self.f(x + t*dr) >= self.f(x) + self.alpha*t*np.inner(self.f_grad(x), dr):
-            if t == 0:
-                print(dr, f(x), self.alpha*t*np.inner(self.f_grad(x), dr))
-                print('line_search failed')
-                break
-            t /= 2
-        return t
-
-    def gradient_step(self):
-        x = self.x[-1]
-        direction = -self.f_grad(x)
-        t = self.line_search(direction)
-        print('t =', t)
-        self.steps.append(t*direction)
-        self.x.append(self.x[-1]+self.steps[-1])
-        return t*direction
-        #gradient method
-    def newton_step(self):
-        x = self.x[-1]
-        step = np.linalg.solve(self.f_hess(x), -self.f_grad(x))
-        self.steps.append(step)
-        self.x.append(self.x[-1]+self.steps[-1])
-        return step
-    def BFGS_step(self):
-        x = self.x[-1]
-        direction = np.linalg.solve(self.B[-1], -self.f_grad(x))
-        t = self.line_search(direction)
-        step = t*direction
-        self.steps.append(step)
-        self.x.append(self.x[-1]+self.steps[-1])
-        y = self.f_grad(self.x[-1]) - self.f_grad(self.x[-2])
-        B_ = self.B[-1]
-        B_0 = np.outer(y,y)/np.inner(y, step)
-        B_1 = - np.outer(B_ @ step, B_ @ step)/(np.transpose(step) @ B_ @ step)
-        #computes next Hessian approximation
-        self.B.append(B_ + B_0 + B_1)
-        return step
-
-    def step(self, method = 'default'):
-        if method == 'default' and self.f_hess != None:
-            if np.array_equal(self.f_hess(self.x[-1]), np.transpose(self.f_hess(self.x[-1]))):
-                try:
-                    np.linalg.cholesky(self.f_hess(self.x[-1]))
-                    method = 'newton'
-                except np.linalg.LinAlgError:
-                    print('non-positive definite matrix')
-        if method == 'BFGS':
-            if len(self.B) == 0:
-                self.B.append(np.identity(self.inputs))
-            self.BFGS_step()
-        elif method == 'newton':
-            self.newton_step()
-        else: # gradient descent is default
-            self.gradient_step()
-        return (self.steps[-1], method)
 
 class Drawer:
     def __init__(self, res = 0.2, margin = 0.2, min_dist = 10e-2):
@@ -169,7 +100,7 @@ def hess_f(entry):
     x, y = entry[0], entry[1]
     return np.array([[-400*(y-x**2)+800*x**2+2, -400*x],[-400*x, 200]])
 
-mim = Minimizer(f, 2, np.array([0, 0]))
+mim = mini.Minimizer(f, 2, np.array([0, 0]))
 mim.f_grad = grad_f
 mim.f_hess = hess_f
 
@@ -186,15 +117,4 @@ vet_f = np.vectorize(lambda in1, in2: f((in1, in2)))
 drawer = Drawer()
 drawer.draw_f(vet_f, mim)
 drawer.draw_path(vet_f, mim, mim.x)
-#count = 0
-#i = 0
-#while i in range(len(mim.x)-1):
-#    j = i+1
-#    while j!=len(mim.x)-1 and np.linalg.norm(mim.x[j] - mim.x[i]) < drawer.min_dist:
-#        j+=1
-#    drawer.draw_curve(vet_f, mim, mim.x[i], mim.x[j], count)
-#    count += np.linalg.norm(mim.x[j]-mim.x[i])
-#    print('path', i, j, 'drawn')
-#    i = j
 drawer.show()
-#fig.show()
