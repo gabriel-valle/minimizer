502b8bb00f0b3e1de14a8df0cfde546296453b6a
Gabriel Valle <gabrielmvalle@gmail.com>
2019-10-15 18:45:51 -0300

    Move drawer class to separated module

--- minimize.py
+++ minimize.py
@@ -1,10 +1,12 @@
 import numpy as np
+import plotly.graph_objects as go
+import math
 class Minimizer:
     def __init__(self, f, inputs, x_0 = np.array([0,0]), alpha = 0.1):
         self.f = f
         self.inputs = inputs
-        self.f_grad = None
-        self.f_hess = None
+        self.f_grad = lambda inp: inp
+        self.f_hess = lambda inp: np.identity(inputs)
         self.x = [x_0]
         self.steps = []
         self.alpha = alpha
@@ -78,3 +80,77 @@ class Minimizer:
                 break
             st = self.step(method=method)
             print(st)
+
+class Drawer:
+    def __init__(self, res = 0.05, margin = 0.2, min_dist = 10e-2):
+        self.fig = None
+        self.res = 0.2
+        self.margin = 0.2
+        self.min_dist = 10e-2/5
+        self.view_area = None
+    def draw_f(self, vet_f, mim):
+        xs = list(map(lambda inp: inp[0], mim.x))
+        ys = list(map(lambda inp: inp[1], mim.x))
+        if self.view_area == None:
+            x_low, x_high = min(xs), max(xs)
+            y_low, y_high = min(ys), max(ys)
+            rangex, rangey = max(abs(x_low), abs(x_high)), max(abs(y_low), abs(y_high))
+            rangex *= 1+self.margin
+            rangey *= 1+self.margin
+            view_area = [[-rangex, -rangey],[rangex, rangey]]
+        X = np.arange(view_area[0][0], view_area[1][0]+self.res, self.res)
+        Y = np.arange(view_area[0][1], view_area[1][1]+self.res, self.res)
+        X, Y = np.meshgrid(X, Y)
+        Z = vet_f(X, Y)
+        surface = go.Figure(data=[go.Surface(x = X, y = Y, z = Z, opacity=0.8)])
+        if self.fig == None:
+            self.fig = surface
+        else:
+            self.fig.data.append(surface.data[0])
+        self.fig.update_layout(title='Function', autosize=True,
+                    width=1000, height=1000,
+                    margin=dict(l=65, r=50, b=65, t=90))
+        return self.fig
+    def draw_curve(self, vet_f, mim, X_0, X_f, color='darkblue'):
+        path = []
+        norm = np.linalg.norm(X_f-X_0)
+        steps = math.ceil(4*norm.item()/self.res)
+        print('steps =', steps, 'norm =', norm.item(), 'steps=', 4*norm.item()/self.res)
+        if steps == 0:
+            return self.fig
+        for t in range(0,steps+1):
+            vet = X_0 + (t/steps)*(X_f-X_0)
+            path.append(list(vet))
+        path = np.array(path)
+        X_line = path[:, 0]
+        Y_line = path[:, 1]
+        Z_line = vet_f(X_line, Y_line)
+        self.fig.add_trace(go.Scatter3d(x=X_line, y=Y_line, z=Z_line, mode='lines',
+            line=dict(
+                color='darkblue',
+                width=10
+            ))
+        )
+        return self.fig
+    def draw_path(self, vet_f, mim, path, color='darkblue'):
+        path = np.array(path)
+        X_scatter = path[:, 0]
+        Y_scatter = path[:, 1]
+        Z_scatter = vet_f(X_scatter, Y_scatter)
+        mark_colors = np.array([0.01*i for i in range(len(X_scatter))])
+        self.fig.add_trace(go.Scatter3d(x=X_scatter, y=Y_scatter, z=Z_scatter,
+            marker=dict(
+                size=3,
+                color=mark_colors,
+                colorscale='Viridis',
+                opacity=0.8
+            ),
+            line=dict(
+                #color=(color+np.sqrt(X_line**2+Y_line**2))/10,
+                color='darkblue',
+                #colorscale='Viridis',
+                width=10
+            ))
+        )
+    def show(self):
+        self.fig.show()
--- proj.py
+++ proj.py
@@ -1,8 +1,5 @@
-import plotly.graph_objects as go
-import plotly.express as px
 import numpy as np
 import pandas as pd
-import math
 import sys
 import minimize as mini
 
@@ -12,82 +9,7 @@ if len(sys.argv) > 1:
 else:
     method = "newton"
 
-print('method:',method)
-
-class Drawer:
-    def __init__(self, res = 0.2, margin = 0.2, min_dist = 10e-2):
-        self.fig = None
-        self.res = 0.2
-        self.margin = 0.2
-        self.min_dist = 10e-2/5
-        self.view_area = None
-    def draw_f(self, vet_f, mim):
-        xs = list(map(lambda inp: inp[0], mim.x))
-        ys = list(map(lambda inp: inp[1], mim.x))
-        if self.view_area == None:
-            x_low, x_high = min(xs), max(xs)
-            y_low, y_high = min(ys), max(ys)
-            rangex, rangey = max(abs(x_low), abs(x_high)), max(abs(y_low), abs(y_high))
-            rangex *= 1+self.margin
-            rangey *= 1+self.margin
-            view_area = [[-rangex, -rangey],[rangex, rangey]]
-        X = np.arange(view_area[0][0], view_area[1][0]+self.res, self.res)
-        Y = np.arange(view_area[0][1], view_area[1][1]+self.res, self.res)
-        X, Y = np.meshgrid(X, Y)
-        Z = vet_f(X, Y)
-        surface = go.Figure(data=[go.Surface(x = X, y = Y, z = Z, opacity=0.8)])
-        if self.fig == None:
-            self.fig = surface
-        else:
-            self.fig.data.append(surface.data[0])
-        self.fig.update_layout(title='Function', autosize=True,
-                    width=1000, height=1000,
-                    margin=dict(l=65, r=50, b=65, t=90))
-        return self.fig
-    def draw_curve(self, vet_f, mim, X_0, X_f, color='darkblue'):
-        path = []
-        norm = np.linalg.norm(X_f-X_0)
-        steps = math.ceil(4*norm.item()/self.res)
-        print('steps =', steps, 'norm =', norm.item(), 'steps=', 4*norm.item()/self.res)
-        if steps == 0:
-            return self.fig
-        for t in range(0,steps+1):
-            vet = X_0 + (t/steps)*(X_f-X_0)
-            path.append(list(vet))
-        path = np.array(path)
-        X_line = path[:, 0]
-        Y_line = path[:, 1]
-        Z_line = vet_f(X_line, Y_line)
-        self.fig.add_trace(go.Scatter3d(x=X_line, y=Y_line, z=Z_line, mode='lines',
-            line=dict(
-                color='darkblue',
-                width=10
-            ))
-        )
-        return self.fig
-    def draw_path(self, vet_f, mim, path, color='darkblue'):
-        path = np.array(path)
-        X_scatter = path[:, 0]
-        Y_scatter = path[:, 1]
-        Z_scatter = vet_f(X_scatter, Y_scatter)
-        mark_colors = np.array([0.01*i for i in range(len(X_scatter))])
-        self.fig.add_trace(go.Scatter3d(x=X_scatter, y=Y_scatter, z=Z_scatter,
-            marker=dict(
-                size=2,
-                color=mark_colors,
-                colorscale='Viridis',
-                opacity=0.8
-            ),
-            line=dict(
-                #color=(color+np.sqrt(X_line**2+Y_line**2))/10,
-                color='darkblue',
-                #colorscale='Viridis',
-                width=10
-            ))
-        )
-    def show(self):
-        self.fig.show() 
-
+print('method:',method) 
 
 # f(x, y) = 100(y-x²)² + (1-x)²
 def f(entry):
@@ -103,9 +25,10 @@ def hess_f(entry):
 mim = mini.Minimizer(f, 2, np.array([0, 0]))
 mim.f_grad = grad_f
 mim.f_hess = hess_f
-mim.iterate(method='newton', log=True)
+mim.iterate(method=method, log=True)
+
 vet_f = np.vectorize(lambda in1, in2: f((in1, in2)))
-drawer = Drawer()
+drawer = mini.Drawer()
 drawer.draw_f(vet_f, mim)
 drawer.draw_path(vet_f, mim, mim.x)
 drawer.show()
